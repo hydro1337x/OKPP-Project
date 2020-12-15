6ee743fedcc8cdbd6d5b258e093ac1f84b961456
Benjamin Mecanović <benjamin.mecanovic@gauss.hr>
2020-12-15 17:52:44 +0100

    Initial

---
+++ Results/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Results
+//
+//  Created by Benjamin Mecanović on 15/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
